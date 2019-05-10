e11e4dda7a81c908b6f8c32710ce1071e79829f1
William Henrotin <whe@odoo.com>
2019-05-10 14:34:56 +0200

    [IMP] mrp: alternative workcenters
    Purpose: link workcenters that can do the same job together.
    Planning a workorder will be done by choosing, among the similar
    workcenters, the one that will finish the production the soonest.
    
    This commit adds a new many2many field on the mrp.workcenter
    model. Planning the workorders will:
     1. Loop on the operation's workcenter and all the alternative ones.
     2. Compute the first available work slot for each of those workcenters.
     3. Choose the soonest finished slot
    
    Task : 1892819

--- addons/mrp/tests/test_workorder_operation.py
+++ addons/mrp/tests/test_workorder_operation.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 import unittest
 from odoo.tests import Form
 from odoo.addons.mrp.tests.common import TestMrpCommon
+from odoo.exceptions import ValidationError
 
 
 class TestWorkOrderProcess(TestMrpCommon):
@@ -13,6 +14,32 @@ class TestWorkOrderProcess(TestMrpCommon):
         super(TestWorkOrderProcess, self).setUp()
         self.source_location_id = self.ref('stock.stock_location_14')
         self.warehouse = self.env.ref('stock.warehouse0')
+        # setting up alternative workcenters
+        self.wc_alt_1 = self.env['mrp.workcenter'].create({
+            'name': 'Nuclear Workcenter bis',
+            'capacity': 3,
+            'time_start': 9,
+            'time_stop': 5,
+            'time_efficiency': 80,
+        })
+        self.wc_alt_2 = self.env['mrp.workcenter'].create({
+            'name': 'Nuclear Workcenter ter',
+            'capacity': 1,
+            'time_start': 10,
+            'time_stop': 5,
+            'time_efficiency': 85,
+        })
+        self.planning_bom = self.env['mrp.bom'].create({
+            'product_id': self.product_4.id,
+            'product_tmpl_id': self.product_4.product_tmpl_id.id,
+            'product_uom_id': self.uom_unit.id,
+            'product_qty': 4.0,
+            'routing_id': self.routing_1.id,
+            'type': 'normal',
+            'bom_line_ids': [
+                (0, 0, {'product_id': self.product_2.id, 'product_qty': 2}),
+                (0, 0, {'product_id': self.product_1.id, 'product_qty': 4})
+            ]})
 
     def test_00_workorder_process(self):
         """ Testing consume quants and produced quants with workorder """
@@ -351,9 +378,9 @@ class TestWorkOrderProcess(TestMrpCommon):
         self.assertEqual(kit_wo.state, 'ready', "Workorder should be in ready state.")
         self.assertEqual(door_wo_1.state, 'ready', "Workorder should be in ready state.")
         self.assertEqual(door_wo_2.state, 'pending', "Workorder should be in pending state.")
-        self.assertEqual(kit_wo.duration_expected, 80, "Workorder duration should be 80 instead of %s." % str(kit_wo.duration_expected))
-        self.assertEqual(door_wo_1.duration_expected, 20, "Workorder duration should be 20 instead of %s." % str(door_wo_1.duration_expected))
-        self.assertEqual(door_wo_2.duration_expected, 20, "Workorder duration should be 20 instead of %s." % str(door_wo_2.duration_expected))
+        self.assertEqual(kit_wo.duration_expected, 960, "Workorder duration should be 960 instead of %s." % str(kit_wo.duration_expected))
+        self.assertEqual(door_wo_1.duration_expected, 480, "Workorder duration should be 480 instead of %s." % str(door_wo_1.duration_expected))
+        self.assertEqual(door_wo_2.duration_expected, 480, "Workorder duration should be 480 instead of %s." % str(door_wo_2.duration_expected))
 
         # subbom: kit for stone tools
         kit_wo.button_start()
@@ -768,3 +795,86 @@ class TestWorkOrderProcess(TestMrpCommon):
         # Check that the workorder is planned now and that it lasts one hour
         self.assertEqual(workorder.date_planned_start, date_start, msg="Workorder should be planned tomorrow.")
         self.assertEqual(workorder.date_planned_finished, date_start + timedelta(hours=1), msg="Workorder should be done one hour later.")
+
+    def test_planning_0(self):
+        """ Test alternative conditions
+        1. alternative relation is directionnal
+        2. a workcenter cannot be it's own alternative """
+        self.workcenter_1.alternative_workcenter_ids = self.wc_alt_1 | self.wc_alt_2
+        self.assertEqual(self.wc_alt_1.alternative_workcenter_ids, self.env['mrp.workcenter'], "Alternative workcenter is not reciprocal")
+        self.assertEqual(self.wc_alt_2.alternative_workcenter_ids, self.env['mrp.workcenter'], "Alternative workcenter is not reciprocal")
+        with self.assertRaises(ValidationError):
+            self.workcenter_1.alternative_workcenter_ids |= self.workcenter_1
+
+    def test_planning_1(self):
+        """ Testing planning workorder with alternative workcenters
+        Plan 6 times the same MO, the workorders should be split accross workcenters
+        The 3 workcenters are free, this test plans 3 workorder in a row then three next.
+        The workcenters have not exactly the same parameters (efficiency, start time) so the
+        the last 3 workorder are not dispatched like the 3 first.
+        At the end of the test, the calendars will look like:
+            - calendar wc1 :[mo1][mo4]
+            - calendar wc2 :[mo2 ][mo5 ]
+            - calendar wc3 :[mo3  ][mo6  ]"""
+        self.workcenter_1.alternative_workcenter_ids = self.wc_alt_1 | self.wc_alt_2
+        workcenters = [self.wc_alt_2, self.wc_alt_1, self.workcenter_1]
+        for i in range(3):
+            # Create an MO for product4
+            mo_form = Form(self.env['mrp.production'])
+            mo_form.product_id = self.product_4
+            mo_form.bom_id = self.planning_bom
+            mo_form.product_qty = 1
+            mo = mo_form.save()
+            mo.action_confirm()
+            mo.button_plan()
+            # Check that workcenters change
+            self.assertEqual(mo.workorder_ids.workcenter_id, workcenters[i], "wrong workcenter")
+
+        for i in range(3):
+            # Planning 3 more should choose workcenters in opposite order as
+            # - wc_alt_2 as the best efficiency
+            # - wc_alt_1 take a little less start time
+            # - workcenter_1 is the worst
+            mo_form = Form(self.env['mrp.production'])
+            mo_form.product_id = self.product_4
+            mo_form.bom_id = self.planning_bom
+            mo_form.product_qty = 1
+            mo = mo_form.save()
+            mo.action_confirm()
+            mo.button_plan()
+            # Check that workcenters change
+            self.assertEqual(mo.workorder_ids.workcenter_id, workcenters[i], "wrong workcenter")
+
+    def test_planning_2(self):
+        """ Plan some manufacturing orders with 2 workorders each
+        Batch size of the operation will influence start dates of workorders
+        The first unit to be produced can go the second workorder before finishing
+        to produce the second unit.
+        calendar wc1 : [q1][q2]
+        calendar wc2 :     [q1][q2]"""
+        self.workcenter_1.alternative_workcenter_ids = self.wc_alt_1 | self.wc_alt_2
+        self.planning_bom.routing_id = self.routing_2
+        # Allow second workorder to start once the first one is not ended yet
+        self.operation_2.batch = 'yes'
+        self.operation_2.batch_size = 1
+        self.env['mrp.workcenter'].search([]).write({'capacity': 1})
+        # workcenters work 24/7
+        self.env['resource.calendar'].search([]).write({'attendance_ids': [(5, False, False)]})
+
+        mo_form = Form(self.env['mrp.production'])
+        mo_form.product_id = self.product_4
+        mo_form.bom_id = self.planning_bom
+        mo_form.product_qty = 2
+        mo = mo_form.save()
+        mo.action_confirm()
+        plan = datetime.now()
+        mo.button_plan()
+        self.assertEqual(mo.workorder_ids[0].workcenter_id, self.wc_alt_2, "wrong workcenter")
+        self.assertEqual(mo.workorder_ids[1].workcenter_id, self.wc_alt_1, "wrong workcenter")
+
+        duration1 = self.operation_2.time_cycle * 100 / self.wc_alt_2.time_efficiency + self.wc_alt_2.time_start
+        duration2 = 2 * self.operation_2.time_cycle * 100 / self.wc_alt_1.time_efficiency + self.wc_alt_1.time_start + self.wc_alt_1.time_stop
+        wo2_start = mo.workorder_ids[1].date_planned_start
+        wo2_stop = mo.workorder_ids[1].date_planned_finished
+        self.assertAlmostEqual(wo2_start, plan + timedelta(minutes=duration1), delta=timedelta(seconds=10), msg="Wrong plannification")
+        self.assertAlmostEqual(wo2_stop, wo2_start + timedelta(minutes=duration2), delta=timedelta(seconds=10), msg="Wrong plannification")
